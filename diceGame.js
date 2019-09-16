9b0a0c54b2d1271730a11fb475333fdfd6e10f16
Adam Neujahr <adamneujahr@gmail.com>
2019-09-16 15:49:52 -0500

    Added diceScore variable to keep track of rollDice multiplier

--- diceGame.js
+++ diceGame.js
@@ -10,8 +10,7 @@ function displayRules(rules){
 	console.log ("Each dice has a different multiplier, which will be laid out below.");
 	console.log ("If you win rock, paper, scissors then you will be able to receive points from the dice you've selected.");
 	console.log ("Remember, don't go over 1000.");
-	console.log ("Good luck!");
-	return
+	return console.log ("Good luck!");
 }
 
 function rollDice(maxSides){
@@ -19,19 +18,49 @@ function rollDice(maxSides){
 }
 
 function pickDice(dice){
-	let fourSide = rollDice(4) * 1
-	let sixSide = rollDice(6) * 2
-	let eightSide = rollDice(8) *4
-	let tenSide = rollDice(10) *8
-	let twelveSide = rollDice(12) * 16
-	let twentySide = rollDice(20) * 32
+	let fourSide = rollDice(4) * 1;
+	let sixSide = rollDice(6) * 2;
+	let eightSide = rollDice(8) * 4;
+	let tenSide = rollDice(10) * 8;
+	let twelveSide = rollDice(12) * 16;
+	let twentySide = rollDice(20) * 32;
+	alert("Type a number of sides you want from the following options:");
+	let diceScore = prompt("4-Sided x 1 multiplier  |  6-Sided x 2 multiplier  |  8-Sided x 4 multiplier  |  10-Sided x 8 multiplier  | 12-Sided x 16 multiplier  |  20-Sided x 32 multiplier");
+		if(diceScore === 4){
+			diceScore = fourSide;
+			return diceScore;
+		}
+		else if (diceScore === 6){
+			diceScore = sixSide;
+			return diceScore;
+		}
+		else if (diceScore === 8){
+			diceScore = eightSide;
+			return diceScore;
+		}
+		else if (diceScore === 10){
+			diceScore = tenSide;
+			return diceScore;
+		}
+		else if (diceScore === 12){
+			diceScore = twelveSide;
+			return diceScore;
+		}
+		else if (diceInt === 20){
+			diceScore = twentySide;
+			return diceScore;
+		}
+		else{
+			alert("Incorrect input.  Try again.")
+			return pickDice()
+		}
 }
 
 
 function playRockPaperScissors(rockPaperScissors){
 	rockPaperScissors = prompt("Rock, paper, or scissors?");
 	rockPaperScissors = rockPaperScissors.toLowerCase();
-	let computerTurn = rollDice(3);  //1 = rock, 2 = paper, 3 = scissors
+	let computerTurn = rollDice(3);  // 1 = rock, 2 = paper, 3 = scissors
 		if (rockPaperScissors === "rock"){
 			if (computerTurn === 1){
 				alert("The computer picked rock.  Try again.");
@@ -49,6 +78,7 @@ function playRockPaperScissors(rockPaperScissors){
 		else if (rockPaperScissors === "paper"){
 			if (computerTurn === 1){
 				alert("The computer picked rock.  You won!");
+				attempts--
 			}
 			else if(computerTurn === 2){
 				alert("The computer picked paper.  Try again.");
@@ -56,14 +86,17 @@ function playRockPaperScissors(rockPaperScissors){
 			}
 			else if(computerTurn === 3){
 				alert("The computer picked scissors.  You lost.  Try again.");
+				attempts--
 			}
 		}
 		else if (rockPaperScissors === "scissors"){
 			if (computerTurn === 1){
 				alert("The computer picked rock.  You lost.  Try again.");
+				attempts--
 			}
 			else if(computerTurn === 2){
 				alert("The computer picked paper.  You won!");
+				attempts--
 			}
 			else if(computerTurn === 3){
 				alert("The computer picked scissors.  Iry again.");
@@ -80,9 +113,10 @@ function playRockPaperScissors(rockPaperScissors){
 function runGame(game){
 	let attempts = 10;
 	let score = 0;
-	while(attempts !== 0){
-		playRockPaperScissors();
-	}
+	pickDice();
+	// while(attempts !== 0){
+	// 	playRockPaperScissors();
+	// }
 	console.log("End")
 }
 
